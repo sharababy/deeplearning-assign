29991044b67b0eb0ad55dbf4de7e1af68daa5055
Vedant Bassi <bassi.vdt@gmail.com>
2018-09-15 17:42:57 +0530

    mlp.py done

--- 7/Training Data and Weights/weights/a.py
+++ 7/Training Data and Weights/weights/a.py
@@ -1,25 +1,43 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2018 g <g@ABCL>
-#
-# Distributed under terms of the MIT license.
-
 import numpy as np
+import csv
+
+X = []
+
+with open('../data.csv') as csvfile:
+	readCSV = csv.reader(csvfile, delimiter=',')
+	next(readCSV)  # Skip header line
+	for row in readCSV:
+		a = list(map(float, row))
+		X.append(a)
+
+X = np.asarray(X)
 
 par1 = np.load('./autoencoder1.npy')
 [W_e_1, b_1, W_d_1, c_1] = par1
 
-
-def error(w, b):
-    err = 0.0
-    for x, y in zip(X, Y):
-        fx = f(w, b, x)
-        err += 0.5*(fx-y)**2
-    return err
-
-
 par2 = np.load('./autoencoder2.npy')
 [W_e_2, b_2, W_d_2, c_2] = par2
-__import__('pdb').set_trace()
+
+
+def f(w,b,x):
+	return 1.0/(1.0 + np.exp(-(np.matmul(x, np.transpose(w)) + b)))
+
+
+def error(we,b,wd,c):
+	err = 0.0
+	for x in X:
+		h1 = f(we,b,x)
+		o1 = f(wd,c,h1)
+		err += np.sum((o1-x)**2)
+	return err
+
+
+e1 = error(W_e_1,b_1,W_d_1,c_1)
+e2 = error(W_e_2, b_2, W_d_2, c_2)
+
+
+print(e1<e2)
+
+
+
+
---
+++ data_gen.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+size = 3
+
+X = np.random.rand(size)
+Y = np.random.rand(size)
+O = np.random.randint(low=0,high=2,size=size) # [low,high)
+
+X = (X*2)-1
+Y = (Y*2)-1
+
+data = []
+
+for x,y,o in zip(X,Y,O):
+
+	data.append([x,y,int(o)])
+
+plt.scatter(X, Y, c=O)
+plt.show()
+
+np.savetxt("foo.csv", data, delimiter=",")
